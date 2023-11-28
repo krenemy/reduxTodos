1bbb9fedb89c9bcbe472723d18236da9c4ea024f
krenemy <aaryaveer.bhardwaj@gmail.com>
2023-11-28 14:10:51 +0530

    addedRemoveTodo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { UseSelector, useSelector } from 'react-redux'
-
+import { removeTodo } from '../features/todoSlice'
+import { useDispatch } from 'react-redux'
 
 function Todos() {
+const dispatch = useDispatch();
 const todos = useSelector((state)=> state.todos)
 console.log(todos)
   return (
     <>
     <h1 className='text-center'>ALLTODOS</h1>
     {todos.map((todo)=>(
-        <div className='w-full bg-black text-white p-4' 
-        key={todo.id}> <h1 className='text-center'>{todo.text}</h1></div> ))}
+        <div className='w-full bg-black text-white p-4 flex justify-between text-center' 
+        key={todo.id}> <h1 className='flex justify-center items-center'>{todo.text}</h1>
+        <button className='bg-white text-black p-4 rounded-md hover:bg-slate-300'
+        onClick={()=>{
+          if(todo.id!==1){
+          dispatch(removeTodo(todo.id))}}}
+        >
+        click</button>
+        </div> ))}
     </>
   )
 }
